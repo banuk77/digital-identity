a0dada5f37221b956ca8b1262dcf5595c278a173
Aleksandr Kotikov <alex.kotikov@lloydsbanking.com>
2018-05-17 12:20:13 +0100

    initial commit

--- app/src/main/java/com/fragilebytes/digitalidentity/MainActivity.java
+++ app/src/main/java/com/fragilebytes/digitalidentity/MainActivity.java
@@ -3,18 +3,20 @@ package com.fragilebytes.digitalidentity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RelativeLayout mainLayout;
+    private RelativeLayout mMainLayout;
+    private ImageView mStampImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mainLayout = findViewById(R.id.main_frame);
+        mMainLayout = findViewById(R.id.main_frame);
+        mStampImg = findViewById(R.id.stampImg);
     }
 
 
