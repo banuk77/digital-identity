d5a3ceadc7839a5073b63d4fac410aa4d1762d11
Aleksandr Kotikov <alex.kotikov@lloydsbanking.com>
2018-05-17 13:24:20 +0100

    nfc added

--- app/src/main/java/com/fragilebytes/digitalidentity/MainActivity.java
+++ app/src/main/java/com/fragilebytes/digitalidentity/MainActivity.java
@@ -3,12 +3,14 @@ package com.fragilebytes.digitalidentity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RelativeLayout mMainLayout;
+    private View mMainLayout;
     private ImageView mStampImg;
 
     @Override
@@ -16,13 +18,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMainLayout = findViewById(R.id.main_frame);
-        mStampImg = findViewById(R.id.stampImg);
+        mStampImg = findViewById(R.id.stamp_img);
         mStampImg.setImageResource(R.drawable.stamp_certified);
+        startJourney();
     }
-
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
+    }
+
+    public void OnClickOutside(View view) {
+        finish();
+    }
+
+    private void startJourney() {
+        mMainLayout.animate().alpha(1).translationY(0).setDuration(600).start();
     }
 }
